8354425c16497da8d5917c4c582c482d8e42295b
Jtsant <jtsant92@hotmail.com>
2022-09-14 15:49:05 +0300

    Params for SSG Data Fetching

--- .next/static/webpack/webpack.3b8095c0b1340d63.hot-update.js
+++ .next/static/webpack/webpack.3b8095c0b1340d63.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "9fbace0061244d3f"; }
+/******/ 	__webpack_require__.h = function() { return "d55542fa01aad472"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
