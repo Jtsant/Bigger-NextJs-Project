c3a1b2066071350913a3beb4c62e2ea6e6677beb
Jtsant <jtsant92@hotmail.com>
2022-09-15 15:38:15 +0300

    MongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,13 +1,26 @@
+import { MongoClient } from "mongodb";
+
 // /api/new-meetup
 // POST /api/new-meetup
 
-function handler(req, res) {
-  if  (req.method === 'POST') {
+async function handler(req, res) {
+  if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      "mongodb+srv://johntsant92:<password>@cluster0.kxzzhqi.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
-
-
 export default handler;
